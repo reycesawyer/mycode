481c4aed57c7b1cc49e64fd0680574f9103b2537
reycesawyer <reycesawyer@yahoo.com>
2023-05-24 15:18:46 -0700

    using main when referencing other scripts in a script

---
+++ if_name/first_module.py
@@ -0,0 +1,11 @@
+print("This code will always execute.")
+
+def main():
+   print("This code belongs to the main function in module 1.")
+
+def sec():
+    print("This command is executing sec from module 1.")
+
+if __name__ == "__main__":
+   main()
+
